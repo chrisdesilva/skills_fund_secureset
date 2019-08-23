1d0c6301b7044dd2c8c83981eb42684859ba411e
Chris DeSilva <chris.a.desilva@gmail.com>
2019-08-23 14:12:23 -0500

    Add padding to banner text

--- src/components/banner.js
+++ src/components/banner.js
@@ -5,8 +5,8 @@ const Banner = props => (
     <div>
         <div className="flex flex-col justify-center items-center relative z-0 bg-gray-100" id="banner">
                 {/* update h2 with school-specific content */}
-                <h2 className="text-3xl lg:text-4xl font-normal text-center mt-8 text-black">Learn Cybersecurity at SecureSet</h2>
-                <h3 className="text-base lg:text-xl font-light text-center text-black">Know exactly what you'll pay with Skills Fund</h3>
+                <h1 className="text-3xl lg:text-4xl font-normal text-center mt-8 text-black px-4">Learn Cybersecurity at SecureSet</h1>
+                <h2 className="text-base lg:text-xl font-light text-center text-black">Know exactly what you'll pay with Skills Fund</h2>
                 <div className="flex flex-col lg:flex-row pb-16">
                     {/* <Button
                         buttonClassName="uppercase bg-secondary p-3 lg:mr-4 w-40 rounded-full shadow-lg text-white"
